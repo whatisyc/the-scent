67c107d3ee5c7fccc6c65adf8ae16869500097b5
Anastasiia Us <nastyaus921@gmail.com>
2026-08-27 15:33:11 -0400

    Add Log In button and the nickname with Log Out button for the logged user

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,30 @@
 import { Link, Outlet } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import supabase from '../client'
 import './Layout.css'
 
 function Layout() {
+
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+
+        const getUser = async () => {
+            const {
+                data: { user }
+            } = await supabase.auth.getUser()
+
+            setUser(user)
+        }
+
+        getUser()
+    }, [])
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
+        setUser(null)
+    }
+
     return(
         <div>
             <nav className="navbar">
@@ -10,6 +33,14 @@ function Layout() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/perfumes">Perfumes</Link></li>
                     <li><Link to="/new-post">Create Post</Link></li>
+                    {user ? (
+                        <li>
+                            <p>{user.user_metadata.nickname}</p>
+                            <button className="nav-button" onClick={handleLogout}>Log Out</button>
+                        </li>
+                    ) : (
+                        <li><Link to="/login">Log In</Link></li>
+                    )}
                 </ul>
             </nav>
             <main>
