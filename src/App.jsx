f2c4ab63f5ae17d4e2b4390cc1ba4f3f942705c5
Anastasiia Us <nastyaus921@gmail.com>
2026-08-27 15:34:06 -0400

    Add Log In and Sign Up page paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import PostPage from "./pages/PostPage"
 import EditPost from "./pages/EditPost"
 import PerfumeGallery from "./pages/PerfumeGallery"
 import PerfumeDetail from "./pages/PerfumeDetail"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="new-post" element={<CreatePost />} />
         <Route path="posts/:id" element={<PostPage />} />
         <Route path="posts/:id/edit" element={<EditPost />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   )
