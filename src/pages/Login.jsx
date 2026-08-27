ab74300386e2784e528dded104517f284e840ceb
Anastasiia Us <nastyaus921@gmail.com>
2026-08-27 15:29:16 -0400

    Add Log In page with link to SignUp page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import supabase from '../client.js'
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        const { error } = await supabase.auth.signInWithPassword({      // {error}
+            email,
+            password
+        })
+
+        if (error) {
+            setErrorMessage(error.message)
+            return
+        }
+
+        window.location.href = "/"
+        }
+
+    return(
+        <div className="login-page">
+            <form onSubmit={handleLogin}>
+                <h1>Log In</h1>
+
+                <label htmlFor="email">Email</label>   
+                <input
+                    id='email'
+                    type='email'
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    required
+                />
+
+                <label htmlFor="password">Password</label>
+                <input
+                    id='password'
+                    type='password'
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    required
+                />
+
+                {errorMessage && <p>{errorMessage}</p>}
+
+                <button type="submit">Log In</button>
+            </form>
+
+            <Link to="/signup">Create an account</Link>
+
+
+        </div>
+    )
+}
+
+export default Login
